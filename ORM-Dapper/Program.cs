46990c2c9940094a16710c404636bf8620753146
Robert Zane <rob.zane24@gmail.com>
2023-01-05 15:57:02 -0600

    Completed ORM-Dapper-Exercise

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+
+            var depts = repo.GetAllDepartments();
+
+            foreach (var dept in depts)
+            {
+                Console.WriteLine(dept.Name);
+                Console.WriteLine(dept.DepartmentID);
+                Console.WriteLine();
+            }
+
+            
+
+            
         }
     }
 }
